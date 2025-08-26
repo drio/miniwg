9155491da791528b88820f4f518aff5702167ef3
David Rio Deiros <driodeiros@gmail.com>
2025-08-26 05:03:39 -0400

    un/marshall structs

--- messages.go
+++ messages.go
@@ -2,6 +2,11 @@
 
 package main
 
+import (
+	"encoding/binary"
+	"errors"
+)
+
 // Protocol constants from WireGuard specification
 const (
 	CONSTRUCTION = "Noise_IKpsk2_25519_ChaChaPoly_BLAKE2s"
@@ -56,5 +61,109 @@ type TransportData struct {
 
 	// Encrypted payload follows (variable length)
 	// Format: encrypted_packet + 16-byte Poly1305 auth tag
+}
+
+// Marshal converts HandshakeInitiation to wire format bytes
+func (msg *HandshakeInitiation) Marshal() []byte {
+	buf := make([]byte, 148)
+	
+	buf[0] = msg.Type
+	// Reserved bytes are already zero
+	binary.LittleEndian.PutUint32(buf[4:8], msg.Sender)
+	
+	copy(buf[8:40], msg.Ephemeral[:])
+	copy(buf[40:88], msg.Static[:])
+	copy(buf[88:116], msg.Timestamp[:])
+	copy(buf[116:132], msg.MAC1[:])
+	copy(buf[132:148], msg.MAC2[:])
+	
+	return buf
+}
+
+// Unmarshal parses wire format bytes into HandshakeInitiation
+func (msg *HandshakeInitiation) Unmarshal(data []byte) error {
+	if len(data) != 148 {
+		return errors.New("invalid handshake initiation length")
+	}
+	
+	msg.Type = data[0]
+	copy(msg.Reserved[:], data[1:4])
+	msg.Sender = binary.LittleEndian.Uint32(data[4:8])
+	
+	copy(msg.Ephemeral[:], data[8:40])
+	copy(msg.Static[:], data[40:88])
+	copy(msg.Timestamp[:], data[88:116])
+	copy(msg.MAC1[:], data[116:132])
+	copy(msg.MAC2[:], data[132:148])
+	
+	return nil
+}
+
+// Marshal converts HandshakeResponse to wire format bytes  
+func (msg *HandshakeResponse) Marshal() []byte {
+	buf := make([]byte, 92)
+	
+	buf[0] = msg.Type
+	// Reserved bytes are already zero
+	binary.LittleEndian.PutUint32(buf[4:8], msg.Sender)
+	binary.LittleEndian.PutUint32(buf[8:12], msg.Receiver)
+	
+	copy(buf[12:44], msg.Ephemeral[:])
+	copy(buf[44:60], msg.Empty[:])
+	copy(buf[60:76], msg.MAC1[:])
+	copy(buf[76:92], msg.MAC2[:])
+	
+	return buf
+}
+
+// Unmarshal parses wire format bytes into HandshakeResponse
+func (msg *HandshakeResponse) Unmarshal(data []byte) error {
+	if len(data) != 92 {
+		return errors.New("invalid handshake response length")
+	}
+	
+	msg.Type = data[0]
+	copy(msg.Reserved[:], data[1:4])
+	msg.Sender = binary.LittleEndian.Uint32(data[4:8])
+	msg.Receiver = binary.LittleEndian.Uint32(data[8:12])
+	
+	copy(msg.Ephemeral[:], data[12:44])
+	copy(msg.Empty[:], data[44:60])
+	copy(msg.MAC1[:], data[60:76])
+	copy(msg.MAC2[:], data[76:92])
+	
+	return nil
+}
+
+// MarshalTransportData converts TransportData header + payload to bytes
+// payload should already be encrypted with auth tag appended
+func MarshalTransportData(receiver uint32, counter uint64, encryptedPayload []byte) []byte {
+	headerSize := 16
+	buf := make([]byte, headerSize+len(encryptedPayload))
+	
+	buf[0] = MessageTypeTransportData
+	// Reserved bytes are already zero  
+	binary.LittleEndian.PutUint32(buf[4:8], receiver)
+	binary.LittleEndian.PutUint64(buf[8:16], counter)
+	
+	copy(buf[headerSize:], encryptedPayload)
+	return buf
+}
+
+// UnmarshalTransportData parses transport message, returns header fields + encrypted payload
+func UnmarshalTransportData(data []byte) (receiver uint32, counter uint64, encryptedPayload []byte, err error) {
+	if len(data) < 16 {
+		return 0, 0, nil, errors.New("transport data too short")
+	}
+	
+	if data[0] != MessageTypeTransportData {
+		return 0, 0, nil, errors.New("not a transport data message")
+	}
+	
+	receiver = binary.LittleEndian.Uint32(data[4:8])
+	counter = binary.LittleEndian.Uint64(data[8:16])
+	encryptedPayload = data[16:]
+	
+	return receiver, counter, encryptedPayload, nil
 }
 
